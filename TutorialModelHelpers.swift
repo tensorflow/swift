dd0c74c91addf202096b4c16ff111d0b67ab7951
Marc Rasi <marcrasi@google.com>
2018-09-07 15:59:56 -0700

    more differentiation cleanup

---
+++ TutorialModelHelpers.swift
@@ -0,0 +1,49 @@
+/// Licensed under the Apache License, Version 2.0 (the "License");
+/// you may not use this file except in compliance with the License.
+/// You may obtain a copy of the License at
+///
+/// https://www.apache.org/licenses/LICENSE-2.0
+///
+/// Unless required by applicable law or agreed to in writing, software
+/// distributed under the License is distributed on an "AS IS" BASIS,
+/// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+/// See the License for the specific language governing permissions and
+/// limitations under the License.
+
+/// This file contains some APIs that are important for the tutorial that are
+/// missing from the standard library. Eventually, we will eliminate this file
+/// by extending the standard library.
+
+// TODO: Add this to the standard library? It's a pretty useful op for
+// classification problems.
+@differentiable(reverse, wrt: (.0), adjoint: adjSparseSoftmaxCrossEntropy)
+@inlinable @inline(__always)
+func sparseSoftmaxCrossEntropy(logits: Tensor<Float>, labels: Tensor<Int32>) -> Float {
+  return Raw.sparseSoftmaxCrossEntropyWithLogits(features: logits, labels: labels).loss.mean()
+}
+
+@inlinable @inline(__always)
+func adjSparseSoftmaxCrossEntropy(logits: Tensor<Float>, labels: Tensor<Int32>, primal: Float, seed: Float) -> Tensor<Float> {
+  return seed * Raw.sparseSoftmaxCrossEntropyWithLogits(features: logits, labels: labels).backprop
+}
+
+// TODO: Necessary because of SR-8699.
+@differentiable(reverse, adjoint: adjAdd)
+@inlinable @inline(__always)
+func add(_ x: Tensor<Float>, _ y: Tensor<Float>) -> Tensor<Float> {
+  return x + y
+}
+
+@inlinable @inline(__always)
+func adjAdd(_ x: Tensor<Float>, _ y: Tensor<Float>, primal: Tensor<Float>, seed: Tensor<Float>) -> (Tensor<Float>, Tensor<Float>) {
+  return (
+    seed.broadcast(like: primal).unbroadcast(like: x),
+    seed.broadcast(like: primal).unbroadcast(like: y))
+}
+
+// TODO: Add this as an extension to `Tensor` as `init(glorotUniform:)`
+@usableFromInline func glorotUniform(_ a: Int32, _ b: Int32) -> Tensor<Float> {
+    let minusOneToOne = 2 * Tensor<Float>(randomUniform: [a, b]) - 1
+    return sqrt(Tensor(6 / Float(a + b))) * minusOneToOne
+}
+
