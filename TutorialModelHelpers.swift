f13865d708088171c0660e3a31fa39062aae3db7
Richard Wei <rxwei@google.com>
2018-09-10 14:23:53 -0700

    Cleanup and improvements

--- TutorialModelHelpers.swift
+++ TutorialModelHelpers.swift
@@ -17,33 +17,22 @@
 // TODO: Add this to the standard library? It's a pretty useful op for
 // classification problems.
 @differentiable(reverse, wrt: (.0), adjoint: adjSoftmaxCrossEntropy)
-@inlinable @inline(__always)
+@inlinable
 func softmaxCrossEntropy(logits: Tensor<Float>, categoricalLabels: Tensor<Int32>) -> Float {
   return Raw.sparseSoftmaxCrossEntropyWithLogits(features: logits, labels: categoricalLabels).loss.mean()
 }
 
-@inlinable @inline(__always)
+@inlinable
 func adjSoftmaxCrossEntropy(logits: Tensor<Float>, categoricalLabels: Tensor<Int32>, primal: Float, seed: Float) -> Tensor<Float> {
   return seed * Raw.sparseSoftmaxCrossEntropyWithLogits(features: logits, labels: categoricalLabels).backprop
 }
 
-// TODO: Necessary because of SR-8699.
-@differentiable(reverse, adjoint: adjAdd)
-@inlinable @inline(__always)
-func add(_ x: Tensor<Float>, _ y: Tensor<Float>) -> Tensor<Float> {
-  return x + y
+extension Tensor where Scalar : BinaryFloatingPoint {
+  @inlinable
+  init(glorotUniform shape: TensorShape) {
+    let minusOneToOne = 2 * Tensor(randomUniform: shape) - 1
+    self = sqrt(Tensor(6 / Scalar(shape.contiguousSize))) * minusOneToOne
+  }
 }
 
-@inlinable @inline(__always)
-func adjAdd(_ x: Tensor<Float>, _ y: Tensor<Float>, primal: Tensor<Float>, seed: Tensor<Float>) -> (Tensor<Float>, Tensor<Float>) {
-  return (
-    seed.broadcast(like: primal).unbroadcast(like: x),
-    seed.broadcast(like: primal).unbroadcast(like: y))
-}
-
-// TODO: Add this as an extension to `Tensor` as `init(glorotUniform:)`
-@usableFromInline func glorotUniform(_ a: Int32, _ b: Int32) -> Tensor<Float> {
-    let minusOneToOne = 2 * Tensor<Float>(randomUniform: [a, b]) - 1
-    return sqrt(Tensor(6 / Float(a + b))) * minusOneToOne
-}
 
