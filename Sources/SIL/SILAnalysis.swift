28ddd896910a3b48288ed7d2003b256b3f0b5413
Adam Paszke <apaszke@google.com>
2019-08-26 15:40:12 -0700

    Add a utility to determine the set of instruction operands

---
+++ Sources/SIL/SILAnalysis.swift
@@ -0,0 +1,50 @@
+public func registersRead(by instr: Instruction) -> [String]? {
+    func unwrap(_ operands: [Operand]) -> [String] {
+        return operands.map { $0.value }
+    }
+    switch instr {
+    case .allocStack(_, _): return []
+    case let .apply(_, value, _, arguments, _): return arguments + [value]
+    case .beginAccess(_, _, _, _, _): return []
+    case let .beginApply(_, value, _, arguments, _): return arguments + [value]
+    case let .beginBorrow(operand): return [operand.value]
+    case let .br(_, operands): return unwrap(operands)
+    case let .builtin(_, operands, _): return unwrap(operands)
+    case let .condBr(cond, _, trueOperands, _, falseOperands):
+        return unwrap(trueOperands) + unwrap(falseOperands) + [cond]
+    case let .condFail(operand, _): return [operand.value]
+    case let .copyAddr(_, value, _, operand): return [value, operand.value]
+    case let .copyValue(operand): return [operand.value]
+    case let .deallocStack(operand): return [operand.value]
+    case let .debugValue(operand, _): return [operand.value]
+    case let .debugValueAddr(operand, _): return [operand.value]
+    case let .destroyValue(operand): return [operand.value]
+    case let .destructureTuple(operand): return [operand.value]
+    case let .endAccess(_, operand): return [operand.value]
+    case let .endApply(value): return [value]
+    case let .endBorrow(operand): return [operand.value]
+    case .floatLiteral(_, _): return []
+    case .functionRef(_, _): return []
+    case let .indexAddr(addr, index): return [addr.value, index.value]
+    case .integerLiteral(_, _): return []
+    case let .load(operand): return [operand.value]
+    case .metatype(_): return []
+    case let .pointerToAddress(operand, _, _): return [operand.value]
+    case let .return(operand): return [operand.value]
+    case let .store(value, operand): return [value, operand.value]
+    case .stringLiteral(_, _): return []
+    case let .struct(_, operands): return unwrap(operands)
+    case let .structElementAddr(operand, _): return [operand.value]
+    case let .structExtract(operand, _): return [operand.value]
+    case let .switchEnum(operand, _): return [operand.value]
+    case let .tuple(elements):
+        switch elements {
+        case let .unlabeled(operands): return unwrap(operands)
+        case let .labeled(_, operands): return operands
+        }
+    case let .tupleExtract(operand, _): return [operand.value]
+    case .unknown(_): return nil
+    case .unreachable: return []
+    case .witnessMethod(_, _, _, _): return []
+    }
+}
