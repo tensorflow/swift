ad4068dbcce26cc9ec28c057dfa5d8d074e13c7a
Eugene Burmako <burmako@google.com>
2019-08-27 14:28:25 -0700

    Fix an error in ModuleTests that was rewriting resources on every test run

--- Tests/SILTests/ModuleTests.swift
+++ Tests/SILTests/ModuleTests.swift
@@ -9,15 +9,23 @@ public final class ModuleTests: XCTestCase {
 
     private func testRoundtrip(_ silPath: String) {
         do {
-            let expected = stripUnsupportedSILSyntax(silPath)
+            guard let expectedData = FileManager.default.contents(atPath: silPath) else {
+                return XCTFail("\(silPath) not found")
+            }
+            guard let expected = String(data: expectedData, encoding: .utf8) else {
+                return XCTFail("\(silPath) not in UTF-8")
+            }
             let module = try Module.parse(fromSILPath: silPath)
             let actual = module.description + "\n"
             if (expected != actual) {
                 if let actualFile = FileManager.default.makeTemporaryFile() {
                     let actualPath = actualFile.path
                     FileManager.default.createFile(atPath: actualPath, contents: Data(actual.utf8))
-                    XCTFail("Roundtrip failed: expected \(silPath), actual: \(actualPath)")
-                    let _ = try? shellout("colordiff", "-u", silPath, actualPath)
+                    if shelloutOrFail("colordiff", "-u", silPath, actualPath) {
+                        XCTFail("Roundtrip failed: expected \(silPath), actual: \(actualPath)")
+                    } else {
+                        XCTFail("Roundtrip failed: expected \(silPath), actual: \n\(actual)")
+                    }
                 } else {
                     XCTFail("Roundtrip failed")
                 }
@@ -25,29 +33,6 @@ public final class ModuleTests: XCTestCase {
         } catch {
             XCTFail(String(describing: error))
         }
-    }
-
-    private func stripUnsupportedSILSyntax(_ silPath: String) -> String {
-        let silURL = URL(fileURLWithPath: silPath)
-        let contents = String(
-            data: FileManager.default.contents(atPath: silPath)!, encoding: .utf8)!
-        let lines = contents.split(separator: "\n", omittingEmptySubsequences: false)
-
-        let noHeader = lines.drop(while: { !$0.hasPrefix("sil ") })
-        let noFooter = noHeader.prefix { !$0.hasPrefix("sil_witness_table ") }
-        let noFullyCommentedLines = noFooter.filter { (line: Substring) -> Bool in
-            let trimmedLeft = line.drop(while: { $0.isWhitespace })
-            return !trimmedLeft.hasPrefix("//")
-        }
-        let noComments = noFullyCommentedLines.map { (line: Substring) -> String in
-            let noComment = line.components(separatedBy: "//")[0]
-            return noComment.replacingOccurrences(
-                of: "\\s+$", with: "", options: .regularExpression)
-        }
-
-        let strippedContents = noComments.joined(separator: "\n")
-        try! strippedContents.write(to: silURL, atomically: true, encoding: .utf8)
-        return strippedContents
     }
 }
 
