1bb2b8afb7b60ca6aa81908098c3f1882645182a
Akshaan Kakar <akshaanCrackers@gmail.com>
2020-05-18 06:56:51 +0530

    Update tutorial to use Epochs API

--- docs/site/tutorials/TutorialDatasetCSVAPI.swift
+++ docs/site/tutorials/TutorialDatasetCSVAPI.swift
@@ -13,34 +13,18 @@
 /// This file contains some special-case APIs that make parts of the tutorial work even though we
 /// do not have general APIs. Soon, we will have general APIs replacing this.
 
-/// Conform `IrisBatch` to `TensorGroup` so that we can load it into a `Dataset`.
-extension IrisBatch: TensorGroup {
-    public static var _typeList: [TensorDataType] = [
-        Float.tensorFlowDataType,
-        Int32.tensorFlowDataType
-    ]
-    public static var _unknownShapeList: [TensorShape?] = [nil, nil]
-    public var _tensorHandles: [_AnyTensorHandle] {
-        fatalError("unimplemented")
-    }
-    public func _unpackTensorHandles(into address: UnsafeMutablePointer<CTensorHandle>?) {
-        address!.advanced(by: 0).initialize(to: features.handle._cTensorHandle)
-        address!.advanced(by: 1).initialize(to: labels.handle._cTensorHandle)
-    }
-    public init(_owning tensorHandles: UnsafePointer<CTensorHandle>?) {
-        features = Tensor(handle: TensorHandle(_owning: tensorHandles!.advanced(by: 0).pointee))
-        labels = Tensor(handle: TensorHandle(_owning: tensorHandles!.advanced(by: 1).pointee))
-    }
-    public init<C: RandomAccessCollection>(_handles: C) where C.Element: _AnyTensorHandle {
-        fatalError("unimplemented")
+/// Conform `IrisBatch` to `Collatable` so that we can load it into a `TrainingEpoch`.
+extension IrisBatch: Collatable {
+    public init<BatchSamples: Collection>(collating samples: BatchSamples)
+        where BatchSamples.Element == Self {
+        features = Tensor<Float>(stacking: samples.map{$0.features})
+        labels = Tensor<Int32>(stacking: samples.map{$0.labels})
     }
 }
 
 /// Initialize an `IrisBatch` dataset from a CSV file.
-extension Dataset where Element == IrisBatch {
-    init(
-        contentsOfCSVFile: String, hasHeader: Bool, featureColumns: [Int], labelColumns: [Int]
-    ) {
+func loadIrisDatasetFromCSV(
+        contentsOfCSVFile: String, hasHeader: Bool, featureColumns: [Int], labelColumns: [Int]) -> [IrisBatch] {
         let np = Python.import("numpy")
 
         let featuresNp = np.loadtxt(
@@ -67,14 +51,6 @@ extension Dataset where Element == IrisBatch {
             fatalError("np.loadtxt result can't be converted to Tensor")
         }
 
-        self.init(elements: IrisBatch(features: featuresTensor, labels: labelsTensor))
-    }
-}
+        return zip(featuresTensor.unstacked(), labelsTensor.unstacked()).map{IrisBatch(features: $0.0, labels: $0.1)}
 
-/// Sequence doesn't have a non-predicated `first` property, so we define one.
-/// TODO: Add this to Swift's stdlib.
-extension Sequence where Element == IrisBatch {
-    var first: IrisBatch? {
-        return first(where: { _ in true })
     }
-}
